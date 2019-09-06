9050967a509d5aa85ee3480a753e8e5e16235a74
dlema <dlema@localhost.localdomain>
2019-09-05 20:12:26 -0500

    Ejercicio para aprender a hacer un commit sobre un repositorio GitHub desde Eclipse

---
+++ bson_validator/src/main/java/com/dlema/bson_validator/App.java
@@ -0,0 +1,13 @@
+package com.dlema.bson_validator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
